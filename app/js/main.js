5fd123206cf850b0439c08e7f4033f0e58774315
Alicia <allxandra@mail.ru>
2015-10-07 21:37:51 +0300

    Страница Связаться со мной верстка

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
-jQuery(document).ready(function($) {
-	console.log('Это главная страница');
-});
+    // Semicolon (;) to ensure closing of earlier scripting
+    // Encapsulation
+    // $ is assigned to jQuery
+    ;(function($) {
+
+         // DOM Ready
+        $(function() {
+
+            // Binding a click event
+            // From jQuery v.1.7.0 use .on() instead of .bind()
+            $('#popup_addproject').bind('click', function(e) {
+
+                // Prevents the default action to be triggered. 
+                e.preventDefault();
+
+                // Triggering bPopup when click event is fired
+                $('.popup').bPopup();
+
+            });
+
+        });
+
+    })(jQuery);
