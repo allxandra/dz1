77217de93eb1389e91cd3d3a4be149902b9d1c6a
Alicia <allxandra@mail.ru>
2015-10-09 20:08:23 +0300

    Правки и попап без close

--- app/js/main.js
+++ app/js/main.js
@@ -21,3 +21,25 @@
         });
 
     })(jQuery);
+
+
+$('.form-input-file-origin').on('change', function(){ // по событию change инпут файла
+    
+    var
+        $this = $(this),
+        val = $this.val(); // берем value у инпут файла (путь загруженого файла)
+    
+    //ч то бы отрезать /fakepath/ (это добавляет сам браузер)
+    
+    var 
+        regexp = /c:\\fakepath\\/gmi, //регулярное выражение для поиска
+        pureVal = val.replace(regexp, ''); // заменяем то что нашли в строке - на пустоту
+    
+    $('.form-input-fake').text(pureVal); // вставляем в блок с текстом имя файла
+
+});
+$('popup').bPopup({
+            closeClass:'.popup-close',
+ });
+
+
